71ec553d26e92b7370078a8c6dba7e60fba00904
petteri.stenius@ubisecure.com <petteri.stenius@ubisecure.com>
2021-06-10 09:28:25 +0300

    cleanup

--- api-cleanup - Copy.ps1
+++
@@ -1,3 +0,0 @@
-﻿$site = Get-SSOObject -Type "site" "iam-academy" -ErrorAction Stop
-$site | Get-SSOLink -LinkName "one" | Select-SSOLink -Link | Remove-SSOObject | Out-Null
-$site | Remove-SSOObject | Out-Null
--- ui-and-ux-demo - Copy.ps1
+++
@@ -1,4 +0,0 @@
-﻿Invoke-RestMethod "https://login.example.ubidemo.com/uas/discovery?entityID=sso-api-sample" | ConvertTo-Json
-Invoke-RestMethod "https://login.example.ubidemo.com/uas/discovery?entityID=cn=Ubilogin,ou=System" | ConvertTo-Json
-Invoke-RestMethod "https://login.example.ubidemo.com/uas/template/default/json" | ConvertTo-Json
-(Invoke-WebRequest "https://login.example.ubidemo.com/uas/template/default/style.css").Content
